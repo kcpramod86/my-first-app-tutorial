5ca4089811b918be2c765d4cde1dacddd3dbba83
kcpramod86 <kcpramod86@gmail.com>
2022-04-26 16:26:41 -0500

    String Interpolation, property binding, Event Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus= "No server was created"
 
-  constructor() { }
+  constructor() {
+    //function(){
+    //}
+    // The arrow function is almost the same as the above expression
+    //() here you passs the argument and {} here you have the function body
+    setTimeout(() => {this.allowNewServer = true}, 2000);
+   }
 
   ngOnInit(): void {
+  }
+  onCreateServer(){
+    this.serverCreationStatus = 'the server was created';
+
+  }
+  onUpdateServerName(event: any){
+
   }
 
 }
